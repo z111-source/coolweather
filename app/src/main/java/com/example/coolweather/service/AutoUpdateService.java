8734a2e141690c8e8d3b4fa202a6cb82e94c58c0
z111-source <2904886296@qq.com>
2023-05-17 17:57:40 +0800

    service

--- app/src/main/java/com/example/coolweather/service/AutoUpdateService.java
+++ app/src/main/java/com/example/coolweather/service/AutoUpdateService.java
@@ -1,9 +1,13 @@
 package com.example.coolweather.service;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.os.SystemClock;
 
 import androidx.annotation.NonNull;
 import androidx.preference.PreferenceManager;
@@ -21,16 +25,21 @@ import okhttp3.Response;
 public class AutoUpdateService extends Service {
 
     @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         updateWeather();
-//        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-//        int anHour = 8 * 60 * 60 * 1000;
-//        long triggerAtTime = SystemClock.elapsedRealtime()+anHour;
-//        Intent i =new Intent(this,AutoUpdateService.class);
-//        PendingIntent pi = PendingIntent.getService(this,0,i,0);
-//        alarmManager.cancel(pi);
-//        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerAtTime,pi);
-//
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        int anHour = 8 * 60 * 60 * 1000;
+        long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
+        Intent i = new Intent(this, AutoUpdateService.class);
+        PendingIntent pi = PendingIntent.getService(this, 0, i, PendingIntent.FLAG_MUTABLE);
+        alarmManager.cancel(pi);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
+
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -40,6 +49,7 @@ public class AutoUpdateService extends Service {
         if (weatherString != null) {
             //有缓存时直接解析天气
             Weather weather = Utility.handleWeatherResponse(weatherString);
+
             String weatherId = weather.basic.weatherId;
             String weatherUrl = "http://guolin.tech/api/weather?cityid="
                     + weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
@@ -64,8 +74,5 @@ public class AutoUpdateService extends Service {
         }
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
+
 }
